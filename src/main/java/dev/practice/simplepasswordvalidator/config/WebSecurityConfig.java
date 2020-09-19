c54e1da67bac3648e13bf1c71916324ff0340666
Marta <martaslysz93@gmail.com>
2020-09-19 22:38:10 +0200

    Added CORS configuration.

--- src/main/java/dev/practice/simplepasswordvalidator/config/WebSecurityConfig.java
+++ src/main/java/dev/practice/simplepasswordvalidator/config/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package dev.practice.simplepasswordvalidator.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                 .authorizeRequests()
                     .anyRequest().permitAll();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("POST");
     }
 
 }
--- src/main/java/dev/practice/simplepasswordvalidator/controller/ValidationController.java
+++ src/main/java/dev/practice/simplepasswordvalidator/controller/ValidationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class ValidationController {
 
     private final ValidationService validationService;
